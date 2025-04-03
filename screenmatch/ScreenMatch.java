26f1fc3ffa956dba5754298448b14c3b49ee19bc
kevinantunezortiz <kevinantunetiz@gmail.com>
2025-04-03 10:42:08 -0600

    se agrego una nueva clase y paquete ScreenMatch donde se agregaron algunas variables de distintos tipos

---
+++ screenmatch/ScreenMatch.java
@@ -0,0 +1,14 @@
+package screenmatch;
+
+public class ScreenMatch {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Pelicula: Matrix");
+        //CamelCase
+        int fechaDeLanzamiento = 1999 ;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+        double media = (8.2+6.0+9.0) / 3;
+        System.out.println(media);
+    }
+}

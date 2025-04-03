12fbb82bf4ae4ea48e13f036d2186d070e54b8a4
kevinantunezortiz <kevinantunetiz@gmail.com>
2025-04-03 15:15:46 -0600

    agregue algunos codigos de uso de formato de strings

--- screenmatch/ScreenMatch.java
+++ screenmatch/ScreenMatch.java
@@ -16,8 +16,18 @@ public class ScreenMatch {
                 Fue lanzada en:
                 """ + fechaDeLanzamiento;
         System.out.println(sinopsis);
+      /*  String nombre = "Maria";
+        int edad = 30;
+        double valor = 55.9999;
+        System.out.println(String.format("Mi nombre es %s, tengo %d años y hoy gasté %.2f dolares", nombre, edad, valor));*/
+        String nombre ="Kevin";
+        int aulas = 4;
 
+        String mensaje= """
+                  Hola, %s!
+                  Bienvenido al curso de Java.
+                  Tendremos %d aulas para mostrarte lo que es necesario para que puedas dar tu primeros pasos en este lenguaje                  """.formatted(nombre, aulas);
 
-
+        System.out.println(mensaje);
     }
 }

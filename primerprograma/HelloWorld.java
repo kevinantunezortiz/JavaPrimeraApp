5598351dee55b0116c4ff6b2a4e8fb97c3e77d52
kevinantunezortiz <kevinantunetiz@gmail.com>
2025-04-03 07:11:07 -0600

    se hizo cambio en el nombre del archivo y se introdujo un nuevo mensaje

--- primerprograma/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package primerprograma;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-    }
-}
---
+++ primerprograma/PrimerPrograma.java
@@ -0,0 +1,7 @@
+package primerprograma;
+
+public class PrimerPrograma {
+    public static void main(String[] args) {
+        System.out.println("\"¡Concluí la aula 01 y ahora estoy sumergiéndome en Java!\".");
+    }
+}

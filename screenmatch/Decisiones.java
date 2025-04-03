ea8a582701b0b59a8bf00b04ac78c192c4d0352e
kevinantunezortiz <kevinantunetiz@gmail.com>
2025-04-03 15:50:53 -0600

    primer ejemplo con if-else en el archivo Decisiones

---
+++ screenmatch/Decisiones.java
@@ -0,0 +1,15 @@
+package screenmatch;
+
+public class Decisiones {
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 2022 ;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        if(fechaDeLanzamiento >= 2022){
+            System.out.println("Peliculas más Populares");
+        }else{
+            System.out.println("Pelicula Retro que aún vale la pena ver");
+        }
+    }
+}
